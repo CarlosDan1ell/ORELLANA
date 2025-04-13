83ff43fb32c302c3fd6269971caa4af79319611e
BlameEllen <105366991+BlameEllen@users.noreply.github.com>
2025-04-13 19:46:11 -0400

    Add files via upload

---
+++ bombacomsensordeumidadedesolo.ino
@@ -0,0 +1,30 @@
+int pinoSensorUmidade = 32;
+int pinoRele = 5; // Pino conectado ao relé
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinoSensorUmidade, INPUT);
+  pinMode(pinoRele, OUTPUT);
+  digitalWrite(pinoRele, LOW); // Relé desligado inicialmente
+}
+
+void loop() {
+  int leitura = analogRead(pinoSensorUmidade);
+  float tensao = leitura * (3.3 / 4095.0); // converte para tensão (ESP32)
+
+  Serial.print("Leitura bruta: ");
+  Serial.print(leitura);
+  Serial.print(" | Tensão: ");
+  Serial.print(tensao);
+  Serial.println(" V");
+
+  if (leitura >= 4000) {
+    digitalWrite(pinoRele, HIGH); // Liga o relé
+    Serial.println(">>> Relé ATIVADO!");
+  } else {
+    digitalWrite(pinoRele, LOW);  // Desliga o relé
+    Serial.println("Relé DESLIGADO.");
+  }
+
+  delay(1000);
+}
